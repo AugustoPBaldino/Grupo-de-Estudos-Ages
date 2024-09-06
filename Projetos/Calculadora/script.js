f6c471c8d444a730d45f5cda7a5d5c84805f856f
Your Name <you@example.com>
2024-09-06 12:56:26 -0300

    Upade final

--- Projetos/Calculadora/script.js
+++ Projetos/Calculadora/script.js
@@ -3,12 +3,8 @@ const operationButtons = document.querySelectorAll("[data-operator]");
 const equalsButton = document.querySelector("[data-equals]");
 const deleteButton = document.querySelector("[data-delete]");
 const allClearButton = document.querySelector("[data-all-clear]");
-const previousOperandTextElement = document.querySelector(
-  "[data-previous-operand]"
-);
-const currentOperandTextElement = document.querySelector(
-  "[data-current-operand]"
-);
+const previousOperandTextElement = document.querySelector("[data-previous-operand]");
+const currentOperandTextElement = document.querySelector("[data-current-operand]");
 
 class Calculator {
   constructor(previousOperandTextElement, currentOperandTextElement) {
